49a4ea64e2b5f4c577e5735c0f13be343cdad916
Boris Lobanov <bob.luckyboy@gmail.com>
2022-02-24 17:18:13 +0300

    added tests + changelog

--- test/fpjs_pro_plugin_test.dart
+++ test/fpjs_pro_plugin_test.dart
@@ -1,18 +1,42 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:fpjs_pro_plugin/fpjs_pro_plugin.dart';
 
 void main() {
-  const MethodChannel channel = MethodChannel('fpjs_pro_plugin');
+  const MethodChannel channel = MethodChannel(FpjsProPlugin.channelName);
+  const testApiKey = 'test_api_key';
+  const testVisitorId = 'test_visitor_id';
 
   TestWidgetsFlutterBinding.ensureInitialized();
 
   setUp(() {
     channel.setMockMethodCallHandler((MethodCall methodCall) async {
-      return '42';
+      if (methodCall.method == 'getVisitorId') {
+        return testVisitorId;
+      }
     });
   });
 
   tearDown(() {
     channel.setMockMethodCallHandler(null);
+  });
+
+  group('getVisitorId', () {
+    test('should throw if called before initialization', () async {
+      expect(() => FpjsProPlugin.getVisitorId(), throwsException);
+    });
+
+    test('should return visitor id when called without tags', () async {
+      await FpjsProPlugin.initFpjs(testApiKey);
+      final result = await FpjsProPlugin.getVisitorId();
+      expect(result, testVisitorId);
+    });
+
+    test('should return visitor id when called without tags', () async {
+      await FpjsProPlugin.initFpjs(testApiKey);
+      final result = await FpjsProPlugin.getVisitorId(
+          tags: {'sessionId': DateTime.now().millisecondsSinceEpoch});
+      expect(result, testVisitorId);
+    });
   });
 }
