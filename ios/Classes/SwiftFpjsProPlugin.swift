d587df4a905f22a2dd967dc7daf91d9a531e6788
Ilya Taratukhin <darkilfa@gmail.com>
2022-08-22 18:44:43 +0300

    feat: update iOS agent to native

--- ios/Classes/SwiftFpjsProPlugin.swift
+++ ios/Classes/SwiftFpjsProPlugin.swift
@@ -1,9 +1,9 @@
 import Flutter
 import UIKit
-import FingerprintJSPro
+import FingerprintPro
 
 public class SwiftFpjsProPlugin: NSObject, FlutterPlugin {
-    var fpjsClient: FingerprintJSProClient?
+    var fpjsClient: FingerprintClientProviding?
 
     public static func register(with registrar: FlutterPluginRegistrar) {
         let channel = FlutterMethodChannel(name: "fpjs_pro_plugin", binaryMessenger: registrar.messenger())
@@ -14,38 +14,64 @@ public class SwiftFpjsProPlugin: NSObject, FlutterPlugin {
     public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
         if (call.method == "init") {
             if let args = call.arguments as? Dictionary<String, Any>,
-               let token = args["apiToken"] as? String {
-                let region = args["region"] as? String
+                let token = args["apiToken"] as? String {
+                let passedRegion = args["region"] as? String
+                var region: Region
+                
+                switch passedRegion {
+                case "eu":
+                    region = .eu
+                case "ap":
+                    region = .ap
+                default:
+                    region = .global
+                }
 
                 var endpoint: URL?
                 if let endpointString = args["endpoint"] as? String {
-                    endpoint = URL(string: endpointString)
+                    region = .custom(domain: endpointString)
                 }
 
-                initFpjs(token: token, endpoint: endpoint, region: region)
+                initFpjs(token: token, region: region)
                 result("Successfully initialized FingerprintJS Pro Client")
             } else {
                 result(FlutterError.init(code: "errorApiToken", message: "missing API Token", details: nil))
             }
         } else if (call.method == "getVisitorId") {
             let args = call.arguments as? Dictionary<String, Any>
-            let tags = args?["tags"] as? FingerprintJSProClient.Tags
+            let tags = args?["tags"] as? [String: JSONType]
+//            var metadata = Metadata()
+            
+//            tags?.forEach({ (tagName: String, tagValue: JSONType) in
+//                metadata.setTag(tagValue, forKey: tagName)
+//            })
 
-            getVisitorId(tags: tags, { (visitorId: String) -> Void in result(visitorId) })
+//            getVisitorId(metadata, { (visitorId: String) -> Void in result(visitorId) })
+            getVisitorId(nil, { (visitorId: String) -> Void in result(visitorId) })
         }
     }
 
-    private func initFpjs(token: String, endpoint: URL? = nil, region: String? = nil) {
-        fpjsClient = FingerprintJSProFactory.getInstance(token: token, endpoint: endpoint, region: region)
+    private func initFpjs(token: String, region: Region) {
+        let configuration = Configuration(apiKey: token, region: region)
+        fpjsClient = FingerprintProFactory.getInstance(configuration)
     }
 
-    private func getVisitorId(tags: FingerprintJSProClient.Tags?, _ handler: @escaping (String) -> Void) {
-        fpjsClient?.getVisitorId { result in
-            switch result {
-            case let .failure(error):
-                handler(error.localizedDescription)
-            case let .success(visitorId):
-                handler(visitorId)
+    private func getVisitorId(_ metadata: Metadata?, _ handler: @escaping (String) -> Void) {
+        NSLog("getVisitorId")
+//        fpjsClient?.getVisitorId { result in
+//            switch result {
+//            case let .failure(error):
+//                handler(error.localizedDescription)
+//            case let .success(visitorId):
+//                handler(visitorId)
+//            }
+//        }
+        Task {
+            do {
+                let visitorId = try await fpjsClient?.getVisitorId(metadata)
+                handler(visitorId ?? "")
+            } catch FPJSError.apiError(let apiError) {
+                handler(apiError.error?.message ?? "")
             }
         }
     }
