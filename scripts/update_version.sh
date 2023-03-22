3b8071014cc9ace1b80419c09bcfe284498f4cc7
Ilya Taratukhin <ilya.taratukhin@fingerprint.com>
2023-03-22 22:11:59 +0100

    chore: remove macos fixed for script

--- scripts/update_version.sh
+++ scripts/update_version.sh
@@ -3,10 +3,10 @@
 echo "VERSION: $VERSION"
 
 
-sed -i '' "s/^version: .*/version: $VERSION/g" pubspec.yaml
-sed -i '' "s/s\.version          = '.*'/s.version          = '$VERSION'/g" ios/fpjs_pro_plugin.podspec
-sed -i '' "s/version '.*'/version '$VERSION'/g" android/build.gradle
-sed -i '' "s/fpjs_pro_plugin: ^.*/fpjs_pro_plugin: ^$VERSION/g" README.md
-sed -i '' "s/const pluginVersion = '.*';/const pluginVersion = '$VERSION';/g" lib/fpjs_pro_plugin.dart
+sed -i "s/^version: .*/version: $VERSION/g" pubspec.yaml
+sed -i "s/s\.version          = '.*'/s.version          = '$VERSION'/g" ios/fpjs_pro_plugin.podspec
+sed -i "s/version '.*'/version '$VERSION'/g" android/build.gradle
+sed -i "s/fpjs_pro_plugin: ^.*/fpjs_pro_plugin: ^$VERSION/g" README.md
+sed -i "s/const pluginVersion = '.*';/const pluginVersion = '$VERSION';/g" lib/fpjs_pro_plugin.dart
 
 
