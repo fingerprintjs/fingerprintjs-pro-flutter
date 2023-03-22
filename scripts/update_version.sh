3e7ddc88cbee6d8e939ec57e2af9969177b4e9cc
Ilya Taratukhin <ilya.taratukhin@fingerprint.com>
2023-03-22 21:57:02 +0100

    chore: introduce semantic-release

---
+++ scripts/update_version.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "VERSION: $VERSION"
+
+
+sed -i '' "s/^version: .*/version: $VERSION/g" pubspec.yaml
+sed -i '' "s/s\.version          = '.*'/s.version          = '$VERSION'/g" ios/fpjs_pro_plugin.podspec
+sed -i '' "s/version '.*'/version '$VERSION'/g" android/build.gradle
+sed -i '' "s/fpjs_pro_plugin: ^.*/fpjs_pro_plugin: ^$VERSION/g" README.md
+sed -i '' "s/const pluginVersion = '.*';/const pluginVersion = '$VERSION';/g" lib/fpjs_pro_plugin.dart
+
+
