1fa24ae3a5e56f790a0294a4e5cf541d1fdce325
Juraj Uhlar <juro.uhlar@gmail.com>
2025-04-04 17:32:20 +0100

    chore: revert Android errors, must wait for newer Android SDK

--- android/src/main/kotlin/com/fingerprintjs/flutter/fpjs_pro/fpjs_pro_plugin/FpjsProPlugin.kt
+++ android/src/main/kotlin/com/fingerprintjs/flutter/fpjs_pro/fpjs_pro_plugin/FpjsProPlugin.kt
@@ -26,8 +26,9 @@ import com.fingerprintjs.android.fpjs_pro.ResponseCannotBeParsed
 import com.fingerprintjs.android.fpjs_pro.NetworkError
 import com.fingerprintjs.android.fpjs_pro.ClientTimeout
 import com.fingerprintjs.android.fpjs_pro.UnknownError
-import com.fingerprintjs.android.fpjs_pro.InvalidProxyIntegrationHeaders
-import com.fingerprintjs.android.fpjs_pro.InvalidProxyIntegrationSecret
+// We must wait for Android SDK 2.9 
+// import com.fingerprintjs.android.fpjs_pro.InvalidProxyIntegrationHeaders
+// import com.fingerprintjs.android.fpjs_pro.InvalidProxyIntegrationSecret
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
@@ -198,8 +199,9 @@ private fun getErrorCode(error: Error): String {
     is ResponseCannotBeParsed -> "ResponseCannotBeParsed"
     is NetworkError -> "NetworkError"
     is ClientTimeout -> "ClientTimeout"
-    is InvalidProxyIntegrationHeaders -> "InvalidProxyIntegrationHeaders"
-    is InvalidProxyIntegrationSecret -> "InvalidProxyIntegrationSecret"
+    // We must wait for Android SDK 2.9 
+    // is InvalidProxyIntegrationHeaders -> "InvalidProxyIntegrationHeaders"
+    // is InvalidProxyIntegrationSecret -> "InvalidProxyIntegrationSecret"
     else -> "UnknownError"
   }
   return errorType
