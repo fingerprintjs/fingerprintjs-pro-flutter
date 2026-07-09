563b2904b763cd7affeca9c09e31ba1e2c43883d
Przemysław Żydek <przemyslawzydek@gmail.com>
2026-07-09 12:13:20 +0200

    chore: update version replacement logic and bump iOS plugin to 4.11.0

--- scripts/update_version.sh
+++ scripts/update_version.sh
@@ -5,7 +5,7 @@ VERSION=$(node -p "require('./package.json').version")
 echo "VERSION: $VERSION"
 
 sed -i "s/^version: .*/version: $VERSION/g" pubspec.yaml
-sed -i "s/s\.version          = '.*'/s.version          = '$VERSION'/g" ios/fpjs_pro_plugin.podspec
+sed -i "s/\"version\": \"[^\"]*\"/\"version\": \"$VERSION\"/g" ios/fpjs_pro_plugin.podspec.json
 sed -i "s/version '.*'/version '$VERSION'/g" android/build.gradle
 sed -i "s/fpjs_pro_plugin: ^.*/fpjs_pro_plugin: ^$VERSION/g" README.md
 sed -i "s/const pluginVersion = '.*';/const pluginVersion = '$VERSION';/g" lib/fpjs_pro_plugin.dart
